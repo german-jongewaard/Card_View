46d985ea3aa09e1d5a47646dd6404ae88b7633dc
german <germanjongewaard@gmail.com>
2017-11-19 23:57:48 +0100

    new files

--- app/src/main/java/dev/com/jongewaard/card_view/adapters/MyAdapter.java
+++ app/src/main/java/dev/com/jongewaard/card_view/adapters/MyAdapter.java
@@ -1,6 +1,7 @@
 package dev.com.jongewaard.card_view.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
